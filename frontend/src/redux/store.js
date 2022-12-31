40bc69c1750bf844f2fecef2102939d6500db171
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2023-01-01 01:05:43 +0530

    can't do more it is complicated now

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartRedux from './cartRedux'
+import userRedux from './userRedux'
 
 
 export const store = configureStore({
   reducer: {
-    cart: cartRedux
+    cart: cartRedux,
+    user: userRedux
   },
 })
